98b6e0ad3a490ba3877589b23ed8489fd20167b3
Praguna Singh <praguna.singh@sap.com>
2017-10-27 10:56:36 +0530

    Adding master script for pom changes

---
+++ masterScript.py
@@ -0,0 +1,35 @@
+""" Master script to be used for making pom changes for patch and release specific tasks.
+    Usage: python masterScript.py <USERID> <CODE> <!Optional:Code specific params>
+    CODES:
+    1: X.XX.X-SNAPSHOT to X.XX.X for master (1st task for milestone)
+    2: X.XX.X to X.XX+1.X-SNAPSHOT for master (2nd pom task for milestone)
+    3: X.XX.X to X.XX+1.X-SNAPSHOT for rel {PARAM: relbranch} (3rd pom task for milestone)
+    4: X.XX.X-SNAPSHOT to X.XX.X+1 for rel {PARAM: relbranch} (1st pom task for patch)
+    5: X.XX.X+1 to X.XX.X+1-SNAPSHOT for rel {PARAM: relbranch} (2nd pom task for patch)"""
+
+import sys
+
+#file imports
+import milestoneRemoveSnapshot
+import milestoneMasterAddSnapshotAndIncrement
+
+USERID = sys.argv[1]
+CODE = sys.argv[2]
+
+if CODE == '1':
+    # X.XX.X-SNAPSHOT to X.XX.X master
+    milestoneRemoveSnapshot.main(USERID)
+elif CODE == '2':
+    # X.XX.X to X.XX+1.X-SNAPSHOT master
+    milestoneMasterAddSnapshotAndIncrement.main(USERID)
+elif CODE == '3':
+    # X.XX.X to X.XX+1.X-SNAPSHOT rel
+    print(CODE)
+elif CODE == '4':
+    # X.XX.X-SNAPSHOT to X.XX.X+1 rel
+    print(CODE)
+elif CODE == '5':
+    # X.XX.X+1 to X.XX.X+1-SNAPSHOT rel
+    print(CODE)
+else:
+    print("Incorrect CODE")
---
+++ milestoneMasterAddSnapshotAndIncrement.py
@@ -0,0 +1,111 @@
+""" Script to do milestone release.
+    Usage: python milestoneMasterAddSnapshotAndIncrement.py <user-id>"""
+
+import subprocess
+import os
+import sys
+import shlex
+from xml.etree import ElementTree
+import shutil
+import json
+
+#VARIABLES
+TEMP_FOLDER_NAME = "Milestone_Release_Folder"
+HONENAME = 'git.wdf.sap.corp'
+PORT = '29418'
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+
+
+REVIEWERS = ['prem.roshan.madhusudhan.nair@sap.com','ananya.mallik@sap.com','girish.sainath@sap.com','sarath.gopal@sap.com','shwetha.kalyanathaya.shashidhara@sap.com']
+
+#UTILITY FUNCTIONS
+def change_parent_pom(filename):
+    #creating ElementTree for the POM file.
+    ElementTree.register_namespace('',"http://maven.apache.org/POM/4.0.0")
+    TREE = ElementTree.parse(filename)
+    ROOT = TREE.getroot()
+    VERSION = ROOT.findall('*')[3]
+    if len(VERSION.text) != 0:
+        temp = VERSION.text.split('.')
+        temp[1] = str(int(temp[1])+1)
+        VERSION.text = '.'.join(temp)
+    else:
+        return -1
+    TREE.write('pom.xml')
+    return 1
+
+def change_nonparent_pom(filename):
+    #creating ElementTree for the POM file.
+    ElementTree.register_namespace('',"http://maven.apache.org/POM/4.0.0")
+    TREE = ElementTree.parse(filename)
+    ROOT = TREE.getroot()
+    VERSION = ROOT.findall('*')[1].findall('*')[3]
+    if len(VERSION.text) != 0:
+        temp = VERSION.text.split('.')
+        temp[1] = str(int(temp[1])+1)
+        VERSION.text = '.'.join(temp)
+    else:
+        return -1
+    TREE.write('pom.xml')
+    return 1
+
+def run_command(command):
+    print("Executing command : %s" % (command))
+    process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
+    (out, err) = process.communicate()
+
+def formReviewerString():
+    """Returns the reviewer string for gerrit review"""
+    result = ""
+    for reviewer in REVIEWERS:
+        if result == "":
+            result = result + "r="+reviewer
+        else:
+            result = result + ",r=" + reviewer
+    return result
+
+def main(USERID):
+    #Read json file containing projects information
+    with open('Projects.json','r') as f:
+        projects = json.load(f)
+
+    #creating a separate directory.
+        os.mkdir(TEMP_FOLDER_NAME)
+        os.chdir(TEMP_FOLDER_NAME)
+
+    #MAIN
+    for project in projects:
+        #cloning master branch
+        CLONECOMMAND = "git clone -b master ssh://"+USERID+"@git.wdf.sap.corp:29418"+project["git"]
+        run_command(CLONECOMMAND)
+        os.chdir(project["projectfolder"])
+
+        #copying commit msg hook file for commit to be possible
+        shutil.copy2(DIR_PATH + "\\commit-msg", ".\\.git\\hooks")
+
+        if project.get("hasparent") is None :
+            if change_parent_pom("pom.xml") == -1:
+                print("********* ERROR *************\nProject %s failed. POM did not contain version." % (project['name']))
+                continue
+        else :
+            #changing parent folder pom
+            if change_parent_pom(project["parentfolder"]+"\\pom.xml") == -1:
+                print("********* ERROR *************\nProject %s failed. POM for folder %s did not contain version." % (project['name'], project['parentfolder']))
+                continue
+            
+            #changing for all subfolder poms
+            for subproject in project["subprojects"]:
+                if change_parent_pom(project["parentfolder"]+"\\pom.xml") == -1:
+                    print("********* ERROR *************\nProject %s failed. POM for folder %s did not contain version." % (project['name'], subproject['folder']))
+                    continue
+
+        #pushing the change to gerrit
+        run_command("git add .")
+        run_command("git commit -m 'MilestoneRelease "+project['name']+"'")
+        pushcommand = "git push ssh://"+USERID+"@"+HONENAME+":"+PORT+project["git"] + " HEAD:refs/for/master%"+formReviewerString()
+        # run_command(pushcommand)
+        os.chdir(DIR_PATH + "\\" + TEMP_FOLDER_NAME)
+
+if __name__ == '__main__':
+    USERID = sys.argv[1]
+    main(USERID)
